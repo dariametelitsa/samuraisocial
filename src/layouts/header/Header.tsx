a257ef15ca44d5f9f9998e0569c14310ccea8657
Daria Metelitsa <dariametelitsa@gmail.com>
2024-05-27 14:06:22 +0300

    second lesson

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,6 +1,7 @@
 import logo from './../../assets/logotype.svg';
 import { Style } from './Header.style.ts';
 import { UserElement } from "../../components/userElement/UserElement.tsx";
+import { SvgIcon } from "../../components/SvgIcon.tsx";
 
 export const Header = () => {
     return (
@@ -8,9 +9,9 @@ export const Header = () => {
             <img src={logo} alt="logotype"/>
             <nav>
                 <ul>
-                    <li><a href={'/'}>img 1</a></li>
-                    <li><a href={'/'}>img 2</a></li>
-                    <li><a href={'/'}>img 3</a></li>
+                    <li><a href={'/'}><SvgIcon iconId={'news'}/></a></li>
+                    <li><a href={'/'}><SvgIcon iconId={'message'}/></a></li>
+                    <li><a href={'/'}><SvgIcon iconId={'notification'}/></a></li>
                 </ul>
                 <div>
                     <UserElement />
